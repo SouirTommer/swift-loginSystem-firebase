f4cc6ccc972c10f577d83d8046bdbf9f279992ad
SouirTommer <souirtommer@gmail.com>
2023-03-28 18:28:58 +0800

    Home and forgot page UI

--- swift-loginSystem-firebase/Controller/HomeController.swift
+++ swift-loginSystem-firebase/Controller/HomeController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class HomeController: UIViewController {
-
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        label.text = "Loading ..."
+        label.numberOfLines = 2
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setupUI()
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        self.view.backgroundColor = .systemBackground
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Logout", style: .plain, target: self, action: #selector(didTapLogout))
+        self.view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        ])
+        
     }
-    */
+    
+    @objc private func didTapLogout() {
 
+        
+    }
 }
