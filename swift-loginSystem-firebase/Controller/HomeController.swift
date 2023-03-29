edcfa1cddc49e80ce52abf09ae3580a1b7fd7572
SouirTommer <souirtommer@gmail.com>
2023-03-29 12:25:52 +0800

    Logout work fine

--- swift-loginSystem-firebase/Controller/HomeController.swift
+++ swift-loginSystem-firebase/Controller/HomeController.swift
@@ -41,7 +41,16 @@ class HomeController: UIViewController {
     }
     
     @objc private func didTapLogout() {
-
-        
+        AuthService.shared.signOut { [weak self] error in
+            guard let self = self else { return }
+            if let error = error {
+                AlertManager.showLogoutErrorAlert(on: self, with: error)
+                return
+            }
+            
+            if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
+                sceneDelegate.checkAuthentication()
+            }
+        }
     }
 }
